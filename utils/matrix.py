0d0dc7d0bb81eece98ed00b54d2e6b7f1c465552
presentj94 <presentj94@gmail.com>
2021-06-02 14:22:43 +0900

    fix: modify progress and add matrix.py

---
+++ utils/matrix.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def submatrix(arr):
+    x, y = np.nonzero(arr)
+    # Using the smallest and largest x and y indices of nonzero elements, 
+    # we can find the desired rectangular bounds.  
+    # And don't forget to add 1 to the top bound to avoid the fencepost problem.
+    return arr[x.min():x.max()+1, y.min():y.max()+1]
