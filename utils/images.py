52c2b04172d8748077ee6f979e996e3633663189
presentj94 <presentj94@gmail.com>
2021-06-14 17:51:42 +0900

    feat: modify images utils codes:

--- utils/images.py
+++ utils/images.py
@@ -9,7 +9,7 @@ def _convert(im):
 
 def show(im):
     plt.axis('off')
-    plt.imshow(_convert(im), interpolation="nearest")
+    plt.imshow(_convert(im), interpolation="bilinear")
     plt.show()
     
 
@@ -23,7 +23,7 @@ def show_patched_image(image, probs_patched_image, probs_original_image, true_la
     text = text1 + '\n' + text2 + '\n' + text3 + '\n' + text4 
     
     plt.axis('off')
-    plt.imshow(_convert(image), interpolation="nearest")
+    plt.imshow(_convert(image), interpolation="bilinear")
     plt.text(100, -5, text,
         horizontalalignment='center',
         verticalalignment='bottom')
@@ -33,7 +33,51 @@ def show_patched_image(image, probs_patched_image, probs_original_image, true_la
 def imshow(imgs, label=""):
     imgs = vutils.make_grid(imgs, normalize=True)
     fig, ax = plt.subplots(1, squeeze=False, frameon=False, dpi=300)
-    ax[0][0].imshow(np.transpose(imgs.cpu().detach().numpy(), (1, 2, 0)), interpolation="nearest")  # type: ignore
+    ax[0][0].imshow(np.transpose(imgs.cpu().detach().numpy(), (1, 2, 0)), interpolation="bilinear")  # type: ignore
     ax[0][0].axis("off")  # type: ignore
     ax[0][0].set_title(label)  # type: ignore
     plt.show(block=True)  # type: ignore
+
+
+def show_tensor(images, title="", text="", block=False):
+    images = vutils.make_grid(images, normalize=True)
+    
+    fig, ax = plt.subplots(1, squeeze=False, frameon=False, dpi=300)
+    ax[0][0].imshow(np.transpose(images.cpu().detach().numpy(), (1,2,0)), interpolation='bilinear')
+    ax[0][0].axis('off')
+    ax[0][0].set_title(title)
+    plt.text(122, -20, text,
+             horizontalalignment='center',
+             verticalalignment='bottom')
+    plt.show(block=block)
+    
+    
+def show_numpy(images, title="", text="", block=False):
+    if len(images) == 1:
+        plt.imshow(images, interpolation="bilinear")
+    else:
+        # set batch case
+        pass
+    
+    plt.axis('off')
+    plt.show(block=block)
+
+
+def show_batch_data(images, labels, title="", block=False):
+    batch = vutils.make_grid(images, nrow=2, padding=20, normalize=True).permute(1,2,0)
+    plt.imshow(batch)
+    plt.axis('off')
+    plt.show(block=block)
+
+
+def test_random_transform():
+    image_shape = [3, 100, 100]
+    
+    image = np.zeros(image_shape)
+    show_numpy(images=image, block=True)
+    
+
+# def reducing_rectangle(image_shape, reduce_rate):
+#     image_size = (image_size**2) * reduce_rate
+#     width = height = int(image_size ** 0.5)
+#     return (3, width, height)
